d01a58c8e91441f34349f321f8593bb87e5d16ca
Gleb Dudarev <glebdydarev@mail.ru>
2022-01-10 14:13:45 -0600

    first commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your farite sport?");
+            string sport = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine($"{name} Loves {color} and {sport}. He's favorite animal is {animal}");
+
+        }
+    }
+}
